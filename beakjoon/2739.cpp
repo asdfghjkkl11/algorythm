29aff41dbbed35126800107c9c5118ec6a8ccff4
Son SeungBeom <asdfghjkkl11@naver.com>
2019-07-21 23:57:08 +0900

    Create 2739.cpp

---
+++ beakjoon/2739.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 1; i <=9;i++) {
+		printf("%d * %d = %d\n",a,i,a*i);
+	}
+	return 0;
+}
