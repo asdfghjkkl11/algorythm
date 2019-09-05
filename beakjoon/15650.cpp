c6fc563ed0ec1cb58ba4386727c13093a848bec2
Son SeungBeom <asdfghjkkl11@naver.com>
2019-09-05 10:03:25 +0900

    Create 15650.cpp

---
+++ beakjoon/15650.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+vector<int> v;
+unordered_map<int, int>um;
+int N, M;
+void DFS(int b) {
+	if (b == M) {
+		for (int i = 0; i<v.size(); i++)
+			printf("%d ", v[i]);
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (um[i] == 1||(!v.empty()&&v.back()>i))
+			continue;
+		um[i]++;
+		v.push_back(i);
+		DFS(b + 1);
+		v.pop_back();
+		um[i]--;
+	}
+}
+int main() {
+	cin >> N >> M;
+	DFS(0);
+	return 0;
+}
